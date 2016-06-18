b4336276e72c2ecd914ec37c55fe88110ce97b52
Hwang <hnaras@naver.com>
2016-06-19 01:40:38 +0900

    리팩토링

--- src/acmicpc/p2000/P2581.java
+++ src/acmicpc/p2000/P2581.java
@@ -4,36 +4,27 @@ import java.util.Scanner;
 
 public class P2581 {
 
-	static int first = 0;
-	static int answer = 0;
-	static boolean isFirst = true;
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
+		int first = 0;
+		int sum = 0;
 		int M = sc.nextInt();
 		int N = sc.nextInt();
+		sc.close();
 		for (int i = M; i <= N; i++) {
-			if(primeCheck(i)) {
-				if (isFirst) first = i;
-				answer += i;
-				isFirst = false;
+			if (primeCheck(i)) {
+				first = first == 0 ? i : first; 
+				sum += i;
 			}
 		}
-		if (answer == 0) {
-			System.out.println(-1);
-		} else {
-			System.out.printf("%d\n%d", answer, first);
-		}
-		
-		sc.close();
+		System.out.println(sum == 0 ? "-1" : (sum + "\n" + first));
 	}
 
 	private static boolean primeCheck(int i) {
 		if (i == 1) return false;
 		for (int k = 2; k <= Math.sqrt(i); k++) {
-			if (i % k == 0) {
-				return false;
-			}
+			if (i % k == 0) return false;
 		}
 		return true;
 	}
