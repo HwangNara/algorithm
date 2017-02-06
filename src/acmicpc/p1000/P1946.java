cac770073a8395614690a38536452e10330116bf
NaraHwang <hnaras@naver.com>
2017-02-06 21:46:21 +0900

    신입사원

--- src/acmicpc/p1000/P1946.java
+++ src/acmicpc/p1000/P1946.java
@@ -1,53 +1,31 @@
 package acmicpc.p1000;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Map;
-import java.util.StringTokenizer;
-import java.util.TreeMap;
+import java.util.Scanner;
 
 public class P1946 {
+	
+	static int MAX = 100000;
+	static int[] e = new int[MAX+1];
 
 	public static void main(String[] args) throws Exception {
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		int N = 0;
-		for (int test_case = 0; test_case < T; test_case++) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
 			
-			String tmpN = br.readLine();
-			if (tmpN == null || tmpN.length() == 0) continue;
+			int N = sc.nextInt();
+			for (int i = 0; i < N; i++) e[sc.nextInt()] = sc.nextInt();
 			
-			N = Integer.parseInt(tmpN);
-			Map<Integer, Integer> map = new TreeMap<>();
-			int[] tests = new int[N + 1];
-			int passMan = 0;
-			boolean isOk = true;
-			StringTokenizer st;
-			for (int i = 0; i < N; i++) {
-				String tmp = br.readLine();
-				if (tmp == null || tmp.length() == 0) continue;
-				st = new StringTokenizer(tmp);
-				map.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+			int ans = 1;
+			int min = e[1];
+			for (int i = 2; i <= N; i++) {
+				if (i == N && e[N] != 1) break;
+				if (min < e[i]) continue;
+				if (min > e[i]) min = e[i];
+				ans++;
 			}
-			
-			int index = 0;
-			for (int key : map.keySet()) {
-				tests[index++] = map.get(key);
-			}
-			
-			for (int manOrder = N - 1; manOrder >= 0; manOrder--) {
-				isOk = true;
-				for (int checkOrder = manOrder - 1; checkOrder >= 0; checkOrder--) {
-					if (tests[checkOrder] < tests[manOrder]) {
-						isOk = false;
-						break;
-					}
-				}
-				if (isOk) passMan++;
-			}
-			
-			System.out.println(passMan);
+			System.out.println(ans);
 		}
+		sc.close();
 	}
 }
