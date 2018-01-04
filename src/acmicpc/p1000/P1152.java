e77dc5a3763c074b7e44c7ed62261d32cfd9fadd
country <hnaras@naver.com>
2018-01-04 21:26:34 +0900

    연속합

--- src/acmicpc/p1000/P1152.java
+++ src/acmicpc/p1000/P1152.java
@@ -1,20 +1,16 @@
 package acmicpc.p1000;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class P1152 {
 
 	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String row = sc.nextLine();
-		row = row.trim();
-		if (row.equals("")) {
-			System.out.println("0");
-		} else {
-			String[] s = row.split(" ");
-			System.out.println(s.length);
-		}
-		sc.close();
+
+        Scanner sc = new Scanner(System.in);
+        String row = sc.nextLine();
+        sc.close();
+        row = row.trim();
+        System.out.println(row.equals("") ? "0" : Arrays.stream(row.split(" ")).filter(str -> !str.equals("")).count());
 	}
 }
--- src/acmicpc/p1000/P1912.java
+++ src/acmicpc/p1000/P1912.java
@@ -1,26 +1,24 @@
 package acmicpc.p1000;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class P1912 {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int max = Integer.MIN_VALUE;
-		int n = sc.nextInt();
-		int[] nums = new int[n];
-		
-		for (int i = 0; i < n; i++) nums[i] = sc.nextInt();
-		
-		for (int i = 0; i < n; i++) {
-			int temp = 0;
-			for (int j = i; j < n; j++) {
-				temp += nums[j];
-				if (temp > max) max = temp;
-			}
-		}
+		int n = Integer.parseInt(br.readLine());
+		int[] dp = new int[n+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= n; i++) {
+            dp[i] = (dp[i-1] > 0 ? dp[i-1] : 0) + Integer.parseInt(st.nextToken());
+            max = Math.max(dp[i], max);
+        }
 		System.out.println(max);
-		sc.close();
 	}
 }
