6f5566a12ebc65706e972b7e1c8c9985c247d28f
country <hnaras@naver.com>
2018-04-15 23:15:10 +0900

    [baekjoon] 빠른 A+B
    https://www.acmicpc.net/problem/15552

---
+++ src/acmicpc/p15000/P15552.java
@@ -0,0 +1,23 @@
+package acmicpc.p15000;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class P15552 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st;
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a+b + "\n");
+        }
+        bw.flush();
+    }
+}
