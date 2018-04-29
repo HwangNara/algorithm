4f8c3aa6174e31135dd84150abf7120fc725fcf0
country <hnaras@naver.com>
2018-04-29 23:42:14 +0900

    baekjoon

--- src/acmicpc/p1000/P1212.java
+++ src/acmicpc/p1000/P1212.java
@@ -1,48 +1,46 @@
 package acmicpc.p1000;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class P1212 {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String n = sc.nextLine();
-		int nSize = n.length();
-		
-		for (int i = 0; i < nSize; i++) {
-			switch( n.charAt(i) ) {
-			case '0' :
-				System.out.print(i == 0 ? "" : "000");
-				break;
-			case '1' :
-				System.out.print(i == 0 ? "1" : "001");
-				break;
-			case '2' :
-				System.out.print(i == 0 ? "10" : "010");
-				break;
-			case '3' :
-				System.out.print(i == 0 ? "11" : "011");
-				break;
-			case '4' :
-				System.out.print("100");
-				break;
-			case '5' :
-				System.out.print("101");
-				break;
-			case '6' :
-				System.out.print("110");
-				break;
-			case '7' :
-				System.out.print("111");
-				break;
-				
-			}
-		}
-		
-		
-		
-		
-		sc.close();
-	}
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String n = br.readLine();
+        int nSize = n.length();
+
+        for (int i = 0; i < nSize; i++) {
+            switch (n.charAt(i)) {
+                case '0':
+                    sb.append(i == 0 ? "0" : "000");
+                    break;
+                case '1':
+                    sb.append(i == 0 ? "1" : "001");
+                    break;
+                case '2':
+                    sb.append(i == 0 ? "10" : "010");
+                    break;
+                case '3':
+                    sb.append(i == 0 ? "11" : "011");
+                    break;
+                case '4':
+                    sb.append("100");
+                    break;
+                case '5':
+                    sb.append("101");
+                    break;
+                case '6':
+                    sb.append("110");
+                    break;
+                case '7':
+                    sb.append("111");
+                    break;
+            }
+        }
+        System.out.println(sb.toString());
+    }
 }
