6772e8dc62d78efa25d69f02b6218eee8874d2ea
country <hnaras@naver.com>
2019-05-05 18:40:18 +0900

    [baekjoon] 수 정렬하기 3

--- src/acmicpc/p10000/P10989.java
+++ src/acmicpc/p10000/P10989.java
@@ -3,24 +3,43 @@ package acmicpc.p10000;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class P10989 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		int N = Integer.parseInt(br.readLine());
 		int[] arr = new int[N];
-		
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = Integer.parseInt(br.readLine());
 		}
-		
-		Arrays.sort(arr);
-		StringBuilder sb = new StringBuilder();
-		for (int i : arr) {
-			sb.append(i + "\n");
+
+		/**
+		 * A[10001] 등장횟수
+		 * B[10001] 누적합
+		 * C[N+1] 답
+		 */
+		int[] A = new int[10001];
+		for (int i = 0; i < arr.length; i++) {
+			A[arr[i]]++;
+		}
+
+		int[]B = new int[10001];
+		B[0] = A[0];
+		for (int i = 1; i < A.length; i++) {
+			B[i] = B[i-1] + A[i];
+		}
+
+		int[]C = new int[N+1];
+		for (int i = arr.length - 1; i >= 0; i--) {
+			C[B[arr[i]]] = arr[i];
+			B[arr[i]]--;
+		}
+
+		for (int i = 1; i < C.length; i++) {
+			sb.append(C[i]);
+			sb.append("\n");
 		}
 		System.out.println(sb.toString());
 	}
