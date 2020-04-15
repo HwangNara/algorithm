92c813ea92386d03952a34c37bba74d7704082c4
country <hnaras@naver.com>
2020-04-15 18:05:30 +0900

    [leetcode] no 9

---
+++ src/leetcode/problem/P9.java
@@ -0,0 +1,16 @@
+package leetcode.problem;
+
+public class P9 {
+
+  public static void main(String[] args) {
+//    int input = -121; // false
+    int input = 10; // false
+//    int input = 121; // true
+    System.out.println(new P9().isPalindrome(input));
+  }
+
+  public boolean isPalindrome(int x) {
+    String origin = String.valueOf(x);
+    return origin.equalsIgnoreCase(new StringBuilder(origin).reverse().toString());
+  }
+}
